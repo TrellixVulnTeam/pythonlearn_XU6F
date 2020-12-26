8dd5fe276889d206732829e24778464140333dfc
sujaychandra <sujaychandraofficial@gmail.com>
2020-12-26 18:10:32 +0530

    third day #3

--- src/DataTypesandOperators/StringMethods.py
+++ src/DataTypesandOperators/StringMethods.py
@@ -12,3 +12,18 @@ print(senetence.split())
 print(senetence.split(' ', 3))
 print(senetence.split('.'))
 print(senetence.split(None,3))
+
+
+# practice
+sentence="practice python for 100 days"
+print(sentence.title())
+print(sentence.istitle())
+print(sentence.lower())
+print(sentence.islower())
+print(sentence.count("a"))
+formated_string="today is {} . day"
+print(formated_string.format("great"))
+print(sentence.split())
+print(formated_string.split(None,2))
+print(formated_string.split(" ",2))
+print(formated_string.split('.'))
