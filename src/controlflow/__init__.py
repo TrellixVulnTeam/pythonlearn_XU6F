26f4ce36fc6b22fb31c7760bc22f19d36b5df00e
sujaychandra <sujaychandraofficial@gmail.com>
2020-12-28 09:28:57 +0530

    #5 boolean expressions and conditional flow

--- src/controlflow/__init__.py
+++ src/controlflow/__init__.py
@@ -1,2 +1,3 @@
-from src.controlflow import IfandElseConditional as iec
+# from src.controlflow import IfandElseConditional as iec
 
+from src.controlflow import BolleanexpressionandCondition as becf
