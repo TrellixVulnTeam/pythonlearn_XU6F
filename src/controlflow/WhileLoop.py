fd29bf4dfdeea61e3dd76466b784804701224c8d
sujaychandra <sujaychandraofficial@gmail.com>
2020-12-28 18:45:26 +0530

    #5 while loop quizes

--- src/controlflow/WhileLoop.py
+++ src/controlflow/WhileLoop.py
@@ -79,3 +79,46 @@ while  current <= number:
 
 # print the factorial of number
 print(product)
+
+
+start_num = 100
+end_num = 200
+count_by = 1
+break_num =start_num
+
+# write a while loop that uses break_num as the ongoing number to
+#   check against end_num
+
+while break_num < end_num:
+      break_num += count_by
+
+print(break_num)
+
+
+
+start_num = 5
+end_num = 100
+count_by = 2
+
+if start_num > end_num:
+    result = "Oops! Looks like your start value is greater than the end value. Please try again."
+
+else:
+    break_num = start_num
+    while break_num < end_num:
+        break_num += count_by
+
+    result = break_num
+
+print(result)
+
+
+
+limit = 40
+
+num = 0
+while (num+1)**2 < limit:
+    num += 1
+nearest_square = num**2
+
+print(nearest_square)
