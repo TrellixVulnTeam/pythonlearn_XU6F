22f4382d00649c84bee92f357d8ee1cee2ff7924
sujaychandra <sujaychandraofficial@gmail.com>
2020-12-31 19:33:38 +0530

    #8 reading and writing file

--- src/scripting/ReadandWritingFile.py
+++ src/scripting/ReadandWritingFile.py
@@ -22,3 +22,46 @@ with open("../data/somefile.txt",'r') as f:
     read_data=f.read()
 
 print(read_data)
+
+
+with open("../data/camolon.txt",'w') as f:
+    data="We're the knights of the round table" \
+         "\nWe dance whenever we're able"
+    f.write(data)
+
+with open("../data/camolon.txt") as f:
+    print(f.read(2))
+    print(f.read(8))
+    print(f.read())
+
+
+with open("../data/camolon.txt") as f:
+    print(f.readline())
+
+with open("../data/camolon.txt") as f:
+    for i in f:
+        print(i)
+
+
+
+def create_cast_list(filename):
+    cast_list = []
+    with open("../data/flying_circus_cast.txt",'a') as f:
+        for i in range(4):
+            name=str(input("enter the cast name"))
+            f.write(name+"\n")
+            cast_list.append(name)
+    #use with to open the file filename
+    #use the for loop syntax to process each line
+    #and add the actor name to cast_list
+
+    # return cast_list
+
+cast_list = create_cast_list('../data/flying_circus_cast.txt')
+with open("../data/flying_circus_cast.txt",'r') as f:
+    for actor in f:
+        print(actor)
+# for actor in cast_list:
+#     print(actor)
+
+
